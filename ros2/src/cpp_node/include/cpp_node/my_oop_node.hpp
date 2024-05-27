ddeb58c74708fab4a3bc115791eaf7fca604de1c
FurkannByrm <frknbyrm05@gmail.com>
2024-05-27 16:40:34 +0300

    path and ros2 packages collacate

---
+++ ros2/src/cpp_node/include/cpp_node/my_oop_node.hpp
@@ -0,0 +1,23 @@
+#ifndef _MY_NODE_HPP__
+#define _MY_NODE_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+
+class MyNode: public rclcpp::Node{
+
+    public: 
+    MyNode();
+    private:
+    int counter_;
+    void callback();
+    rclcpp::TimerBase::SharedPtr timer_;
+
+
+
+
+};
+
+
+
+
+#endif
