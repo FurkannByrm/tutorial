552e762e4322a8ee9c86a4e0ce90c78ee09fd27d
FurkannByrm <frknbyrm05@gmail.com>
2024-05-29 22:44:51 +0300

    **

--- cpp/necati ergin homework/src/work.cpp
+++ cpp/necati ergin homework/src/work.cpp
@@ -5,7 +5,11 @@ int main()
 {
     std::cout<<"hello"<<std::endl;
 
-
+ 
+    constexpr int x = 3;
+    // decltype((x)) r = x;
+    constexpr int y = x;
+    std::cout<<y<<std::endl;
     return 0;
 }
 
